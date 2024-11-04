e69ec4751b47b8f731214b2fdfb616458b71c14e
Sidhartha Reddy <kattasidharthareddy22it@student.vardhaman.org>
2024-11-04 17:53:16 +0530

    added base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/siviskar-final/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
